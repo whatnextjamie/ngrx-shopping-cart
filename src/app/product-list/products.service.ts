ab364a6bada5d580494d6ab1ce91226ee9f80b34
Jamie Thompson <jamieraethompson@gmail.com>
2023-02-18 15:45:38 -0600

    created service to get product list data from API

--- src/app/product-list/products.service.ts
+++ src/app/product-list/products.service.ts
@@ -1,8 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root',
-})
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Product } from './products.model';
+
+@Injectable({ providedIn: 'root' })
 export class ProductsService {
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  getProducts(): Observable<Array<Product>> {
+    return this.http
+      .get<{ items: Product[] }>('/api/products')
+      .pipe(map((products) => products.items || []));
+  }
 }
